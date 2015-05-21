0e89802f07d7e3ec19c1b0421afbb5e176f39612
xyclos <jakej587@gmail.com>
2015-05-21 13:55:23 -0400

    clean up addManagedAuthorization.rb

--- Clients/AddSearchSelectClients/addClient.rb
+++ Clients/AddSearchSelectClients/addClient.rb
@@ -1,10 +1,10 @@
 /class name			:class_name			:class
-css selector		:css	 
-id					:id	 
+css selector		:css
+id					:id
 link text			:link_text			:link
-name				:name	 
-partial link text	:partial_link_text	 
-tag name			:tag_name	 
+name				:name
+partial link text	:partial_link_text
+tag name			:tag_name
 xpath				:xpath/
 
 require 'selenium-webdriver'
@@ -22,85 +22,85 @@ require 'selenium-webdriver'
 	def teardown
 		@driver.quit
 	end
-	
+
 	def run
 		setup
 		yield
 		teardown
 	end
-	
+
 	def login(username,password)
 		puts 'Logging in'
-	
+
 		@driver.find_element(id: 'cred_userid_inputtext').send_keys username # 'rhadevadmin@rhadev.onmicrosoft.com'
 		@driver.find_element(id: 'cred_password_inputtext').send_keys password # 'RHAdev9891'
 		@driver.find_element(id: 'cred_sign_in_button').click
 		sleep(1)
 		@driver.find_element(id: 'cred_sign_in_button').click
 	end
-	
+
 	def click_go_then_click_add
 		@driver.find_element(css: 'input[value=Go]').click
 		@driver.find_element(css: 'a[href="/clinical/clients/create"]').click
 	end
-	
+
 	def navigate_to_clients_page
 		@driver.find_element(link_text: 'Clients').click
 	end
-	
+
 	def fill_in_personal_information
 		puts 'Filling in personal information'
-	
-		@driver.find_element(id: "PersonalInfo_NameFirst").send_keys "Automationfirstname1" #First name
-		@driver.find_element(id: "PersonalInfo_NameMiddle").send_keys "Automationmiddlename1" #Middle name
-		@driver.find_element(id: "PersonalInfo_NameLast").send_keys "Automationlastname1" #Last name
+
+		@driver.find_element(id: "PersonalInfo_NameFirst").send_keys "Simon" #First name
+		@driver.find_element(id: "PersonalInfo_NameMiddle").send_keys "Charles" #Middle name
+		@driver.find_element(id: "PersonalInfo_NameLast").send_keys "Lane" #Last name
 		@driver.find_element(id: "PersonalInfo_NameSuffix").send_keys "Auto" #Suffix
 		@driver.find_element(id: "PersonalInfo_PreviousName").send_keys "Automationpreviousname Lastname"
 		@driver.find_element(id: "PersonalInfo_NamePreferred").send_keys "Automationpreferredname Lastname"
 		@driver.find_element(id: "PersonalInfo_SSN").click
 		@driver.find_element(id: "PersonalInfo_SSN").click
-		
+
 		@driver.find_element(id: "PersonalInfo_SSN").send_keys "111111111"
 		@driver.find_element(id: "PersonalInfo_Birthdate").click
-		
+
 		@driver.find_element(id: "PersonalInfo_Birthdate").send_keys "03/30/1990"
 		@driver.find_element(id: "PersonalInfo_EclipseId").send_keys "1"
-		
+
 		gender_drop_down = @driver.find_element(xpath: '/html/body/div/div/div/form/section[1]/div[4]/div[1]/div/div/div[1]/input')
 		gender_drop_down.click
 		gender_drop_down.send_keys 'Female'
 		gender_drop_down.send_keys :return
-		
+
 		race_drop_down_1 = @driver.find_element(xpath: '/html/body/div/div/div/form/section[1]/div[4]/div[2]/div/div/div[1]/input')
 		race_drop_down_1.click
 		race_drop_down_1.send_keys 'Automation Race1'
 		race_drop_down_1.send_keys :return
-		
+
 		race_drop_down_2 = @driver.find_element(xpath: '/html/body/div/div/div/form/section[1]/div[4]/div[3]/div/div/div[1]/input')
 		race_drop_down_2.click
 		race_drop_down_2.send_keys 'Automation Race1'
 		race_drop_down_2.send_keys :return
-		
+
 		ethnic_origin_drop_down = @driver.find_element(xpath: '/html/body/div/div/div/form/section[1]/div[5]/div[1]/div/div/div[1]/input')
 		ethnic_origin_drop_down.click
 		ethnic_origin_drop_down.send_keys 'Automation Ethnic Origin1'
 		ethnic_origin_drop_down.send_keys :return
-		
+
 		county_of_origin_drop_down = @driver.find_element(xpath: "/html/body/div/div/div/form/section[1]/div[5]/div[2]/div/div/div[1]/input")
 		county_of_origin_drop_down.click
 		county_of_origin_drop_down.send_keys 'Alamance'
 		county_of_origin_drop_down.send_keys :return
-		
+
 		primary_language_drop_down = @driver.find_element(xpath: '/html/body/div/div/div/form/section[1]/div[6]/div[1]/div/div/div[1]/input')
 		primary_language_drop_down.click
 		primary_language_drop_down.send_keys 'Automation Language1'
 		primary_language_drop_down.send_keys :return
-		
+
 		secondary_language_drop_down = @driver.find_element(xpath: '/html/body/div/div/div/form/section[1]/div[6]/div[2]/div/div/div[1]/input')
 		secondary_language_drop_down.click
 		secondary_language_drop_down.send_keys 'Automation Language1'
 		secondary_language_drop_down.send_keys :return
-		
+
 		tertiary_language_drop_down = @driver.find_element(xpath: '/html/body/div/div/div/form/section[1]/div[6]/div[3]/div/div/div[1]/input')
 		tertiary_language_drop_down.click
 		tertiary_language_drop_down.send_keys 'Automation Language1'
@@ -108,10 +108,10 @@ require 'selenium-webdriver'
 
 		@driver.find_element(id: "PersonalInfo_EthnicInfoNotes").send_keys 'This client info was added by automaton.'
 	end
-	
+
 	def fill_in_contact_information
 		puts 'Filling in contact information'
-		
+
 		# have to comment this out for now because of a bug disabling the county of residence field after clicking this https://www.pivotaltracker.com/story/show/93660984
 		#@driver.find_element(xpath: "/html/body/div[1]/div/div/form/section[2]/div[1]/div/div/label[2]/span").click #Is Homeless switch
 		#@driver.find_element(xpath: "/html/body/div[1]/div/div/form/section[2]/div[1]/div/div/label[2]/span").click #Sets "Is Homeless" back to NO
@@ -132,23 +132,23 @@ require 'selenium-webdriver'
 		@driver.find_element(id: "ContactInfo_HomePhone").click #Home number focus
 		@driver.find_element(id: "ContactInfo_HomePhone").send_keys "1112223333" #Enters Home number
 		@driver.find_element(id: "ContactInfo_WorkPhone").click #Work number focus
-		@driver.find_element(id: "ContactInfo_WorkPhone").send_keys "4445556677" #Enters Work number	
+		@driver.find_element(id: "ContactInfo_WorkPhone").send_keys "4445556677" #Enters Work number
 		@driver.find_element(id: "ContactInfo_EmailAddress").send_keys "automated1@email.com" #Email address
 
 		@driver.find_element(xpath: "/html/body/div/div/div/form/section[2]/div[6]/div[2]/div/div/div[1]/input").click #Select Preferred Communication drop down
 		@driver.find_element(xpath: "/html/body/div/div/div/form/section[2]/div[6]/div[2]/div/div/div[1]/input").send_keys "Cell" #Enters a selection
 		@driver.find_element(xpath: "/html/body/div/div/div/form/section[2]/div[6]/div[2]/div/div/div[1]/input").send_keys :return #Selects the selection
-						
+
 		# These are only accessible when "is homeless" is true
 		#@driver.find_element(id: "ContactInfo_PrimaryShelter").send_keys "Home" #Primary shelter
 		#@driver.find_element(id: "ContactInfo_SecondaryShelter").send_keys "Gun range" #Secondary shelter
 
 		@driver.find_element(id: "ContactInfo_ContactInfoComments").send_keys "This contact info was added by automation."
 	end
-	
+
 	def fill_in_emergency_contact_information
 		puts 'Filling in emergency contact information'
-	
+
 		@driver.find_element(id: "EmergencyContact_NameFirst").send_keys "Autoemergencyname1" #First name
 		@driver.find_element(id: "EmergencyContact_NameMiddle").send_keys "Autoemergencymiddlename1" #Middle name
 		@driver.find_element(id: "EmergencyContact_NameLast").send_keys "Autoemergencylastname1" #Last name
@@ -157,39 +157,39 @@ require 'selenium-webdriver'
 		@driver.find_element(xpath: "//*[@id='guardian-info-section']/div[5]/div[1]/div/div/div[1]/input").click #Relationship drop down
 		@driver.find_element(xpath: "//*[@id='guardian-info-section']/div[5]/div[1]/div/div/div[1]/input").send_keys "Automation Relationship1" #enters a selection
 		@driver.find_element(xpath: "//*[@id='guardian-info-section']/div[5]/div[1]/div/div/div[1]/input").send_keys :return #selects the selection
-		
+
 		@driver.find_element(id: "EmergencyContact_EmailAddress").send_keys "automatedemergency@email.com" #Email address for emergency contact
 		@driver.find_element(id: "EmergencyContact_HomePhone").click #Home number focus
 		@driver.find_element(id: "EmergencyContact_HomePhone").send_keys "1111111111" #Enters Emergency Contact Home number
 		@driver.find_element(id: "EmergencyContact_CellPhone").click #Work number focus
-		@driver.find_element(id: "EmergencyContact_CellPhone").send_keys "2222222222" #Enters Emergency Contact Cell number	
+		@driver.find_element(id: "EmergencyContact_CellPhone").send_keys "2222222222" #Enters Emergency Contact Cell number
 	end
-	
+
 	def save_client_and_validate_address
 		@driver.find_element(css: 'button[type="submit"].btn.btn-primary').click
 		@driver.find_element(xpath: "//*[@id='address-selection-vue']/div/div/div/div[3]/button[2]/span").click
 	end
-	
+
 	def logout
 		puts 'Logging out'
 		@driver.find_element(css: 'a.dropdown-toggle i.caret').click
 		@driver.find_element(css: 'a[href="/account/signout"]').click
 	end
-	
+
 	run do
 		# 1. Login
 		login USERNAME, PASSWORD
-		
+
 		# 2. Navigate to Clients Page
 		navigate_to_clients_page
-		
+
 		# 3. Add a client.
 		click_go_then_click_add
 		fill_in_personal_information
 		fill_in_contact_information
 		fill_in_emergency_contact_information
 		save_client_and_validate_address
-		
+
 		# 4. Logout
 		logout
 	end
--- Clients/Authorizations/addManagedAuthorization.rb
+++ Clients/Authorizations/addManagedAuthorization.rb
@@ -1,127 +1,179 @@
 /class name			:class_name			:class
-css selector		:css	 
-id					:id	 
+css selector		:css
+id					:id
 link text			:link_text			:link
-name				:name	 
-partial link text	:partial_link_text	 
-tag name			:tag_name	 
+name				:name
+partial link text	:partial_link_text
+tag name			:tag_name
 xpath				:xpath/
 
-#BROWSER: FIREFOX
-require "rubygems"
 require "selenium-webdriver"
- browser = Selenium::WebDriver.for :firefox
-  browser.get "https://rha.azurewebsites.net/"
-  #browser.window.resize_to(1680,1050)
-  #driver.manage.timeouts.implicit_wait = 10
-   browser.current_url
-	browser.title
 
-	#Login screen
-		browser.find_element(id: "cred_userid_inputtext").send_keys "rhadevadmin@rhadev.onmicrosoft.com"
-		browser.find_element(id: "cred_password_inputtext").send_keys "RHAdev989"
-		browser.find_element(id: "cred_sign_in_button").click
+	USERNAME = 'rhadevadmin@rhadev.onmicrosoft.com'
+	PASSWORD = 'RHAdev9891'
+	ENVIRONMENT_UNDER_TEST = 'https://rha.azurewebsites.net/'
+
+	def setup
+		@driver = Selenium::WebDriver.for :firefox
+		@driver.navigate.to ENVIRONMENT_UNDER_TEST
+		@driver.manage.timeouts.implicit_wait = 10
+		@wait = Selenium::WebDriver::Wait.new(timeout: 6)
+	end
+
+	def teardown
+		@driver.quit
+	end
+
+	def run
+		setup
+		yield
+		teardown
+	end
+
+    def login(username,password)
+		puts 'Logging in'
+
+		@driver.find_element(id: 'cred_userid_inputtext').send_keys username # 'rhadevadmin@rhadev.onmicrosoft.com'
+		@driver.find_element(id: 'cred_password_inputtext').send_keys password # 'RHAdev9891'
+		@driver.find_element(id: 'cred_sign_in_button').click
 		sleep(1)
-		browser.find_element(id: "cred_sign_in_button").click
+		@driver.find_element(id: 'cred_sign_in_button').click
+	end
 
-	#Navigate to the Clients' page, clicks Authorizations on left nav, and then click Add Authorization button
-		#Clicks Clients
-			browser.find_element(link_text: "Clients").click
-		#Searches and accesses a specific client
-			browser.find_element(id: "search").send_keys "Automation"
-				browser.find_element(xpath: "/html/body/div[1]/div/div/form/div[2]/div/div/span/input").click
-					browser.find_element(link_text: "Automationfirstname1 Automationlastname1").click
+    def logout
+		puts 'Logging out'
+		@driver.find_element(css: 'a.dropdown-toggle i.caret').click
+		@driver.find_element(css: 'a[href="/account/signout"]').click
+	end
 
-				#Clicks Authorizations on the left nav
-					browser.find_element(link_text: "Authorizations").click
+    def navigate_to_clients_page
+		puts 'Navigating to Clients page'
+		@driver.find_element(link_text: 'Clients').click
+	end
 
-				#Clicks Add Authorizations
-					browser.find_element(xpath: "/html/body/div[1]/div[2]/div/div[3]/div/div[3]/a").click
+    def navigate_to_a_client(name)
+		puts "Navigating to a Client: #{name}"
+		@driver.find_element(id: 'search').send_keys name
+		@driver.find_element(css: 'input[value=Go]').click
+		@driver.find_element(link_text:  name).click
+	end
 
-				#Authorization type
-					browser.find_element(xpath: "//*[@id='authorization-form']/div[1]/div/div/div[1]/label/span").click #managed
-						#browser.find_element(xpath: "//*[@id='authorization-request-form']/div[1]/div/div/div[2]/label/span").click
+    def navigate_to_authorizations
+        puts 'Navigating to Authorizations'
+        @driver.find_element(link_text: "Authorizations").click
+    end
 
-			#Insurance
-				browser.find_element(xpath: "//*[@id='request-info']/div/div[1]/div/div/div[1]/input").click #Insurance drop down
-				browser.find_element(xpath: "//*[@id='request-info']/div/div[1]/div/div/div[1]/input").send_keys "Automationpayer Lastname" #enters a selection
-					browser.find_element(xpath: "//*[@id='request-info']/div/div[1]/div/div/div[1]/input").send_keys :return #selects the selection
+    def click_add_authorization
+        puts 'Clicking Add Authorization'
+        @driver.find_element(css: "a[href=\"/clinical/clients/1/authorizations/create\"]").click
+    end
 
-			#Request Date
-				browser.find_element(id: "ManagedAuthorizationInputs_DateRequested").click
-					browser.find_element(id: "ManagedAuthorizationInputs_DateRequested").send_keys "02/13/2015"
+    def set_authorization_type(type)
+        puts "Setting authorization type to #{type}"
+        @driver.find_element(css: "input[value=\"#{type}\"]")
+    end
 
-			#Effective To
-				browser.find_element(id: "Effective_To").click
-					browser.find_element(id: "Effective_To").send_keys "12/25/2015"
+    run do
+        # 1. Login
+		login USERNAME, PASSWORD
 
-			#Effective From
-				browser.find_element(id: "Effective_From").click
-					browser.find_element(id: "Effective_From").send_keys "02/13/2015"
+        # 2. Navigate to Authorizations
+        navigate_to_clients_page
+        navigate_to_a_client 'Lloyd Christmas'
+        navigate_to_authorizations
 
-			#Units Requested
-				browser.find_element(id: "ManagedAuthorizationInputs_UnitsRequested").click #Field focus units requested
-				browser.find_element(id: "ManagedAuthorizationInputs_UnitsRequested").click #Field focus units requested
-					browser.find_element(id: "ManagedAuthorizationInputs_UnitsRequested").send_keys "50" #enters units requested amount
-
-			#Request Date
-				browser.find_element(id: "ManagedAuthorizationInputs_DateRequested").click
-					browser.find_element(id: "ManagedAuthorizationInputs_DateRequested").send_keys "02/13/2015"
-
-			#Service
-				browser.find_element(xpath: "//*[@id='unbundled-services']/div/div/div/div[1]/input").click #Insurance drop down
-				browser.find_element(xpath: "//*[@id='unbundled-services']/div/div/div/div[1]/input").send_keys "HCPCS - AddedByAutomationService1" #enters a selection
-					browser.find_element(xpath: "//*[@id='unbundled-services']/div/div/div/div[1]/input").send_keys :return #selects the selection
-sleep(1)
-			#Referring NPI Info (Optional)
-				browser.find_element(xpath: "//*[@id='referring-provider']/section/div/div[1]/div/div/div[1]/input").click #Referring Provider drop down
-				browser.find_element(xpath: "//*[@id='referring-provider']/section/div/div[1]/div/div/div[1]/input").send_keys "Referralautomation" #enters a selection
-					browser.find_element(xpath: "//*[@id='referring-provider']/section/div/div[1]/div/div/div[1]/input").send_keys :return #selects the selection
-sleep(1)
-				#Saves Authorization
-					browser.find_element(xpath: "//*[@id='authorization-form']/div[3]/ul/li[2]/button").click
-sleep(1)
-#Opens Authorization again to fill out Received information
-		
-		#Clicks Authorizations on the left nav
-			browser.find_element(link_text: "Authorizations").click
-
-		#Accesses the added authorization
-			browser.find_element(link_text: "Automationpayer Lastname1").click
-
-			#Enters info for received authorization
-				browser.find_element(id: "Number").send_keys "00001"
-
-			#Status
-				browser.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[1]/div[3]/div/div/div[1]/input").click #Insurance drop down
-				browser.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[1]/div[3]/div/div/div[1]/input").send_keys "Active" #enters a selection
-					browser.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[1]/div[3]/div/div/div[1]/input").send_keys :return #selects the selection	
-
-				#Units Authorized
-					browser.find_element(id: "UnitsAuthorized").click #Field focus units requested
-						browser.find_element(id: "UnitsAuthorized").click #Field focus units requested
-							browser.find_element(id: "UnitsAuthorized").send_keys "20" #enters units authorized amount
-
-				#Date Recieved
-					browser.find_element(id: "DateReceived").click
-						browser.find_element(id: "DateReceived").send_keys "02/15/2015"
-							
-				#Limits
-					browser.find_element(id: "VueLimit_Value").click #Field focus units requested
-						browser.find_element(id: "VueLimit_Value").click #Field focus units requested
-							browser.find_element(id: "VueLimit_Value").send_keys "30" #enters limits amount
-
-				#Status
-					browser.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[1]/div[2]/div/div/div[1]/input").click #Insurance drop down
-						browser.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[1]/div[2]/div/div/div[1]/input").send_keys "Day" #enters a selection
-							browser.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[1]/div[2]/div/div/div[1]/input").send_keys :return #selects the selection	
-
-					#Adds Limit
-						browser.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[2]/div/a").click
-
-						#Saves
-							browser.find_element(xpath: "//*[@id='authorization-form']/div/ul/li[2]/button").click
-
-browser.close
+        # 3. Add an Authorization
+        click_add_authorization
+        set_authorization_type 'Managed'
+    end
 
 
+    				#Clicks Authorizations on the left nav
+    					@driver.find_element(link_text: "Authorizations").click
+
+    				#Clicks Add Authorizations
+    					@driver.find_element(xpath: "/html/body/div[1]/div[2]/div/div[3]/div/div[3]/a").click
+
+    				#Authorization type
+    					@driver.find_element(xpath: "//*[@id='authorization-form']/div[1]/div/div/div[1]/label/span").click #managed
+    						#@driver.find_element(xpath: "//*[@id='authorization-request-form']/div[1]/div/div/div[2]/label/span").click
+
+    			#Insurance
+    				@driver.find_element(xpath: "//*[@id='request-info']/div/div[1]/div/div/div[1]/input").click #Insurance drop down
+    				@driver.find_element(xpath: "//*[@id='request-info']/div/div[1]/div/div/div[1]/input").send_keys "Automationpayer Lastname" #enters a selection
+    					@driver.find_element(xpath: "//*[@id='request-info']/div/div[1]/div/div/div[1]/input").send_keys :return #selects the selection
+
+    			#Request Date
+    				@driver.find_element(id: "ManagedAuthorizationInputs_DateRequested").click
+    					@driver.find_element(id: "ManagedAuthorizationInputs_DateRequested").send_keys "02/13/2015"
+
+    			#Effective To
+    				@driver.find_element(id: "Effective_To").click
+    					@driver.find_element(id: "Effective_To").send_keys "12/25/2015"
+
+    			#Effective From
+    				@driver.find_element(id: "Effective_From").click
+    					@driver.find_element(id: "Effective_From").send_keys "02/13/2015"
+
+    			#Units Requested
+    				@driver.find_element(id: "ManagedAuthorizationInputs_UnitsRequested").click #Field focus units requested
+    				@driver.find_element(id: "ManagedAuthorizationInputs_UnitsRequested").click #Field focus units requested
+    					@driver.find_element(id: "ManagedAuthorizationInputs_UnitsRequested").send_keys "50" #enters units requested amount
+
+    			#Request Date
+    				@driver.find_element(id: "ManagedAuthorizationInputs_DateRequested").click
+    					@driver.find_element(id: "ManagedAuthorizationInputs_DateRequested").send_keys "02/13/2015"
+
+    			#Service
+    				@driver.find_element(xpath: "//*[@id='unbundled-services']/div/div/div/div[1]/input").click #Insurance drop down
+    				@driver.find_element(xpath: "//*[@id='unbundled-services']/div/div/div/div[1]/input").send_keys "HCPCS - AddedByAutomationService1" #enters a selection
+    					@driver.find_element(xpath: "//*[@id='unbundled-services']/div/div/div/div[1]/input").send_keys :return #selects the selection
+    sleep(1)
+    			#Referring NPI Info (Optional)
+    				@driver.find_element(xpath: "//*[@id='referring-provider']/section/div/div[1]/div/div/div[1]/input").click #Referring Provider drop down
+    				@driver.find_element(xpath: "//*[@id='referring-provider']/section/div/div[1]/div/div/div[1]/input").send_keys "Referralautomation" #enters a selection
+    					@driver.find_element(xpath: "//*[@id='referring-provider']/section/div/div[1]/div/div/div[1]/input").send_keys :return #selects the selection
+    sleep(1)
+    				#Saves Authorization
+    					@driver.find_element(xpath: "//*[@id='authorization-form']/div[3]/ul/li[2]/button").click
+    sleep(1)
+    #Opens Authorization again to fill out Received information
+
+    		#Clicks Authorizations on the left nav
+    			@driver.find_element(link_text: "Authorizations").click
+
+    		#Accesses the added authorization
+    			@driver.find_element(link_text: "Automationpayer Lastname1").click
+
+    			#Enters info for received authorization
+    				@driver.find_element(id: "Number").send_keys "00001"
+
+    			#Status
+    				@driver.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[1]/div[3]/div/div/div[1]/input").click #Insurance drop down
+    				@driver.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[1]/div[3]/div/div/div[1]/input").send_keys "Active" #enters a selection
+    					@driver.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[1]/div[3]/div/div/div[1]/input").send_keys :return #selects the selection
+
+    				#Units Authorized
+    					@driver.find_element(id: "UnitsAuthorized").click #Field focus units requested
+    						@driver.find_element(id: "UnitsAuthorized").click #Field focus units requested
+    							@driver.find_element(id: "UnitsAuthorized").send_keys "20" #enters units authorized amount
+
+    				#Date Recieved
+    					@driver.find_element(id: "DateReceived").click
+    						@driver.find_element(id: "DateReceived").send_keys "02/15/2015"
+
+    				#Limits
+    					@driver.find_element(id: "VueLimit_Value").click #Field focus units requested
+    						@driver.find_element(id: "VueLimit_Value").click #Field focus units requested
+    							@driver.find_element(id: "VueLimit_Value").send_keys "30" #enters limits amount
+
+    				#Status
+    					@driver.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[1]/div[2]/div/div/div[1]/input").click #Insurance drop down
+    						@driver.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[1]/div[2]/div/div/div[1]/input").send_keys "Day" #enters a selection
+    							@driver.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[1]/div[2]/div/div/div[1]/input").send_keys :return #selects the selection
+
+    					#Adds Limit
+    						@driver.find_element(xpath: "//*[@id='authorization-form']/section[2]/div[2]/rha-limits/div/div[2]/div/a").click
+
+    						#Saves
+    							@driver.find_element(xpath: "//*[@id='authorization-form']/div/ul/li[2]/button").click
