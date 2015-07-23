dcd0595682e7b4451e4b6d13c6dd6736d17b834a
xyclos <jakej587@gmail.com>
2015-07-23 09:14:22 -0400

    fix setup client data script

--- setup_client_data.sh
+++ setup_client_data.sh
@@ -1,14 +1,34 @@
-export USERNAME='rhadevadmin@rhadev.onmicrosoft.com'
-export PASSWORD='RHAdev9892'
-export ENVIRONMENT_UNDER_TEST='https://rha.azurewebsites.net/'
-export CLIENT_FIRST_NAME='Jordan'
-export CLIENT_LAST_NAME='Moore'
-ruby /Users/jakejohnson/Development/rha-automation/Clients/AddSearchSelectClients/addClient.rb &
-wait
-ruby /Users/jakejohnson/Development/rha-automation/Clients/Referrals/addReferral.rb &
-wait
-ruby /Users/jakejohnson/Development/rha-automation/Clients/Assignments/addAssignments.rb &
-wait
-ruby /Users/jakejohnson/Development/rha-automation/Clients/Payers/addPayers.rb &
-wait
-ruby /Users/jakejohnson/Development/rha-automation/Clients/Authorizations/addManagedAuthorization.rb
+if [ -z "$1" ]
+    then
+        echo "No arguments supplied. Usage: $0 <client_first_name> <client_last_name>"
+else
+    DIR=$(pwd)
+    export USERNAME='rhadevadmin@rhadev.onmicrosoft.com'
+    export PASSWORD='RHAdev9892'
+    export ENVIRONMENT_UNDER_TEST='https://rha.azurewebsites.net/'
+    export CLIENT_FIRST_NAME=$1
+    export CLIENT_LAST_NAME=$2
+    cd Clients/AddSearchSelectClients/
+    ruby addClient.rb &
+    cd $DIR
+    wait
+    cd Clients/Referrals/
+    ruby addReferral.rb &
+    cd $DIR
+    wait
+    cd Clients/Assignments/
+    ruby addAssignments.rb &
+    cd $DIR
+    wait
+    cd Clients/Payers/
+    ruby addPayers.rb &
+    cd $DIR
+    wait
+    cd Clients/Authorizations/
+    ruby addManagedAuthorization.rb &
+    cd $DIR
+    wait
+    cd Clients/ClinicalCharts/
+    ruby addClinicalProgressNote.rb &
+    cd $DIR
+fi
